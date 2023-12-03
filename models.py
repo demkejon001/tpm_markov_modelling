69a5c1534a328f4a00d4c62c852dc19eda7c64d3
Jonathan Demke <demkejon001@gmail.com>
2023-12-02 17:29:26 -0700

    made our models more modular

--- models.py
+++ models.py
@@ -48,42 +48,78 @@ class AutoencodingWorldModel(nn.Module):
     def __init__(self, 
                  lr, 
                  hidden_layers=None, 
-                 latent_dim=None, 
+                 transition_layers=None,
                  weight_decay=0., 
                  conv_dropout_p=.05, 
                  fc_dropout_p=.1, 
                  batch_norm=False, 
                  dropout=False):
         super().__init__()
+        self.action_dim = 4
         if hidden_layers is None:
             hidden_layers = [16, 32, 64]
-        if latent_dim is None:
-            latent_dim = hidden_layers[-1]
+            
+        if len(hidden_layers) > 3:
+            raise ValueError(f"hidden_layers can have at max 3 elements: Yours contains {len(hidden_layers)}")
+                
+        if len(hidden_layers) == 3:
+            self.obs_enc_dim = hidden_layers[-1]        
+        if len(hidden_layers) == 2:
+            self.obs_enc_dim = 3*3 * hidden_layers[-1]
+        if len(hidden_layers) == 1:
+            self.obs_enc_dim = 5*5 * hidden_layers[-1]
         
-        self.latent_dim = latent_dim
-        self.obs_enc_dim = hidden_layers[-1]
+        if transition_layers is None:
+            transition_layers = [hidden_layers[-1], hidden_layers[-1]]
         
-        self.state_encoder = nn.Sequential(
-            ConvLayer(3, hidden_layers[0], kernel_size=3, batch_norm=batch_norm, dropout=dropout, dropout_p=conv_dropout_p),
-            ConvLayer(hidden_layers[0], hidden_layers[1], kernel_size=3, batch_norm=batch_norm, dropout=dropout, dropout_p=conv_dropout_p),
-            # ConvLayer(hidden_layers[1], hidden_layers[1], kernel_size=3, padding=1, batch_norm=batch_norm, dropout=dropout, dropout_p=conv_dropout_p),
-            ConvLayer(hidden_layers[1], hidden_layers[2], kernel_size=3, batch_norm=batch_norm, dropout=dropout, dropout_p=conv_dropout_p),
-        )
+        self.state_encoder = self.initialize_state_encoder(hidden_layers, batch_norm, dropout, conv_dropout_p)
 
-        self.transition_model = nn.Sequential(NonLinearLayer(self.obs_enc_dim + 4, hidden_layers[-1], batch_norm=batch_norm, dropout=dropout, dropout_p=fc_dropout_p),
-                                      NonLinearLayer(hidden_layers[-1], hidden_layers[-1], batch_norm=batch_norm, dropout=dropout, dropout_p=fc_dropout_p),
-                                      NonLinearLayer(hidden_layers[-1], self.latent_dim, batch_norm=False, dropout=False))
+        self.transition_model = self.initialize_transition_model(transition_layers, batch_norm, dropout, fc_dropout_p)
 
-        self.state_decoder = nn.Sequential(
-            ConvTransposeLayer(self.latent_dim, hidden_layers[1], kernel_size=3, batch_norm=batch_norm, dropout=dropout, dropout_p=conv_dropout_p), 
-            ConvTransposeLayer(hidden_layers[1], hidden_layers[0], kernel_size=3, batch_norm=batch_norm, dropout=dropout, dropout_p=conv_dropout_p), 
-            # nn.ConvTranspose2d(hidden_layers[0], hidden_layers[0], kernel_size=3, padding=1), 
-            # nn.SiLU(),
-            nn.ConvTranspose2d(hidden_layers[0], 3, kernel_size=3), 
-            nn.Tanh(),
-        )
+        self.state_decoder = self.initialize_state_decoder(hidden_layers, batch_norm, dropout, conv_dropout_p)
         
         self.optim = torch.optim.Adam(self.parameters(), lr=lr, weight_decay=weight_decay)
+        
+        self.model_name = f"autoencoding_s_{'_'.join(map(str, hidden_layers))}_t_{'_'.join(map(str, transition_layers))}"
+        
+    def initialize_state_encoder(self, hidden_layers, batch_norm, dropout, conv_dropout_p) -> nn.Module:
+        state_encoder = [ConvLayer(3, hidden_layers[0], kernel_size=3, batch_norm=batch_norm, dropout=dropout, dropout_p=conv_dropout_p)]
+        for i in range(len(hidden_layers) - 1):
+            state_encoder.append(ConvLayer(hidden_layers[i], hidden_layers[i+1], kernel_size=3, batch_norm=batch_norm, dropout=dropout, dropout_p=conv_dropout_p))
+            
+        return nn.Sequential(*state_encoder)
+    
+    def initialize_transition_model(self, transition_layers, batch_norm, dropout, fc_dropout_p) -> nn.Module:
+        transition_model = []
+        all_transition_layers = [self.obs_enc_dim + self.action_dim] + transition_layers + [self.obs_enc_dim]
+        for i in range(len(all_transition_layers)-1):
+            if i == (len(all_transition_layers)-2): # if last layer don't do dropout
+                transition_model.append(NonLinearLayer(all_transition_layers[i], all_transition_layers[i+1], batch_norm=batch_norm))
+            else:
+                transition_model.append(NonLinearLayer(all_transition_layers[i], all_transition_layers[i+1], batch_norm=batch_norm, dropout=dropout, dropout_p=fc_dropout_p))
+        return nn.Sequential(*transition_model)
+    
+    def initialize_state_decoder(self, hidden_layers, batch_norm, dropout, conv_dropout_p) -> nn.Module:
+        if len(hidden_layers) == 3:
+            state_decoder = [
+                ConvTransposeLayer(self.obs_enc_dim, hidden_layers[1], kernel_size=3, batch_norm=batch_norm, dropout=dropout, dropout_p=conv_dropout_p), 
+                ConvTransposeLayer(hidden_layers[1], hidden_layers[0], kernel_size=3, batch_norm=batch_norm, dropout=dropout, dropout_p=conv_dropout_p), 
+                nn.ConvTranspose2d(hidden_layers[0], 3, kernel_size=3), 
+                nn.Tanh(),
+            ]
+        if len(hidden_layers) == 2:
+            state_decoder = [
+                ConvTransposeLayer(self.obs_enc_dim, hidden_layers[0], kernel_size=5, batch_norm=batch_norm, dropout=dropout, dropout_p=conv_dropout_p), 
+                nn.ConvTranspose2d(hidden_layers[0], 3, kernel_size=3), 
+                nn.Tanh(),
+            ]
+        if len(hidden_layers) == 1:
+            state_decoder = [
+                nn.ConvTranspose2d(self.obs_enc_dim, 3, kernel_size=7), 
+                nn.Tanh(),
+            ]
+            
+        return nn.Sequential(*state_decoder, nn.Tanh())
         
     def forward(self, state, action):
         state_encoding = self.get_state_encoding(state)
@@ -100,7 +136,7 @@ class AutoencodingWorldModel(nn.Module):
     def get_state_encoding(self, state):
         B, C, H, W = state.shape
         state_encoding = self.state_encoder(state)
-        return state_encoding.squeeze(-1).squeeze(-1)
+        return state_encoding.flatten(1)
     
     def training_step(self, state, action, r, t, next_state, eval=False):
         B = state.shape[0]
@@ -129,22 +165,23 @@ class AutoencodingWorldModel(nn.Module):
         return self.training_step(s, a, r, t, ns, eval=True)
 
     @torch.no_grad()
-    def obs_acc(self, pred_obs, true_obs, threshold=20):
+    def obs_acc(pred_obs, true_obs, threshold=20):
         def pp(data):
             return (data + .5) * 255
         B, C, H, W = pred_obs.shape
         obs_diff = torch.abs(pp(pred_obs) - pp(true_obs))
-        obs_diff = obs_diff.sum(axis=1)
-        return (torch.sum(obs_diff < threshold) / (B * H * W)).item()
+        obs_diff = obs_diff.sum(axis=1) < threshold
     
     @torch.no_grad()
-    def agent_pos_acc(self, pred_obs, true_obs, threshold=20):
+    def agent_pos_acc(pred_obs, true_obs, threshold=20):
         def pp(data):
-            return (data + .5) * 255
+            rgb = (data + .5) * 255
+            return torch.clamp(rgb[:, 2] - (rgb[:, 0] + rgb[:, 1]), 0, 255)
+        
         B, C, H, W = pred_obs.shape
         obs_diff = torch.abs(pp(pred_obs) - pp(true_obs))
-        obs_diff = obs_diff[:, 2].sum(axis=(1, 2))
-        return (torch.sum(obs_diff < threshold) / B).item()
+        obs_diff = torch.clamp((obs_diff < threshold).sum(axis=(1, 2)) - H*W + 1, 0, 1)
+        return torch.mean(obs_diff.float())
 
 
 class SeparatedAutoencodingWorldModel(AutoencodingWorldModel):
